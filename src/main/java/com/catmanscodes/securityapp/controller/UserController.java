9934c32c2911e5d8dfb4218dc27cfba1e90d2716
catMan <vimal.maru10083@gmail.com>
2026-09-04 14:19:12 +0530

    added oauth2 google code

--- src/main/java/com/catmanscodes/securityapp/controller/UserController.java
+++ src/main/java/com/catmanscodes/securityapp/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.catmanscodes.securityapp.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("api/v1/users")
 public class UserController {
 
     @GetMapping("/hi")
@@ -16,5 +23,16 @@ public class UserController {
         return "Hello there!";
     }
 
+    @GetMapping("/profile")
+    public Map<String, Object> getProfile(@AuthenticationPrincipal OAuth2User userProfile) {
+
+        Map<String, Object> profile = new HashMap<>();
+
+        profile.put("name", userProfile.getAttribute("name"));
+        profile.put("email", userProfile.getAttribute("email"));
+        profile.put("picture", userProfile.getAttribute("picture"));
+
+        return profile;
+    }
 
 }
